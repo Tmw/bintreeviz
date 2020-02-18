bddabb7ec7ee0e21ec9aa7f910d1530682dadb41
Tiemen Waterreus <tiemen@tiemenwaterreus.com>
2020-02-18 22:57:01 +0100

    Use Textmatrix instead

--- lib/ascii_renderer.ex
+++ lib/ascii_renderer.ex
@@ -11,39 +11,34 @@ defmodule Bintreeviz.AsciiRenderer do
   alias Bintreeviz.Node
 
   def render(%Node{} = root) do
-    IO.write(IO.ANSI.clear())
-    do_render(root)
-
-    # figure out how tall the tree is by taking the tree depth and multiplying
-    # it very naively with 5 ( 3 lines for the node itself + 2 for the margin).
-    # Move the cursor there so any input that is writting to STDOUT after
-    # wont mess up the drawn tree.
-    bottom = Bintreeviz.Layouthelper.tree_depth(root) * 5
-    IO.write(IO.ANSI.cursor(bottom, 0))
+    buffer = Textmatrix.new()
+    buffer = do_render(root, buffer)
+    IO.puts(buffer)
   end
 
-  defp do_render(nil), do: nil
+  defp do_render(nil, buffer), do: buffer
 
-  defp do_render(%Node{} = root) do
+  defp do_render(%Node{} = root, buffer) do
+    line = make_line_for_node(root)
+
+    buffer =
+      buffer
+      |> Textmatrix.write(root.x + @offset_x, root.y + @offset_y, line)
+      |> Textmatrix.write(root.x + @offset_x, root.y + @offset_y + 1, "| #{root.label} |")
+      |> Textmatrix.write(root.x + @offset_x, root.y + 2 + @offset_y, line)
+
+    buffer = do_render(root.left_child, buffer)
+    do_render(root.right_child, buffer)
+  end
+
+  defp make_line_for_node(%Node{} = node) do
     line =
-      root
+      node
       |> node_width()
       |> Kernel.-(2)
       |> repeat_char("-")
 
-    line = "+#{line}+"
-
-    IO.write(IO.ANSI.cursor(root.y + @offset_y, root.x + @offset_x))
-    IO.write("#{line}")
-
-    IO.write(IO.ANSI.cursor(root.y + 1 + @offset_y, root.x + @offset_x))
-    IO.write("| #{root.label} |\n")
-
-    IO.write(IO.ANSI.cursor(root.y + 2 + @offset_y, root.x + @offset_x))
-    IO.write("#{line}\n")
-
-    do_render(root.left_child)
-    do_render(root.right_child)
+    "+#{line}+"
   end
 
   # return the node's label length + padding + border.
@@ -53,21 +48,5 @@ defmodule Bintreeviz.AsciiRenderer do
 
   defp repeat_char(times, char) do
     for _i <- 0..(times - 1), do: char
-  end
-end
-
-defmodule Bintreeviz.Layouthelper do
-  @moduledoc """
-  Internal module with helper functions for calculating the height of the tree
-  """
-  def tree_depth(root, level \\ 0)
-
-  def tree_depth(nil, level), do: level
-
-  def tree_depth(root, level) do
-    max(
-      tree_depth(root.left_child, level + 1),
-      tree_depth(root.right_child, level + 1)
-    )
   end
 end
