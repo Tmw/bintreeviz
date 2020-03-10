41a0336c2d51542df1c27280f79b10ab59e24032
Tiemen Waterreus <tiemen@tiemenwaterreus.com>
2020-03-10 23:58:39 +0100

    Working on drawing connections between nodes

--- lib/ascii_renderer.ex
+++ lib/ascii_renderer.ex
@@ -11,34 +11,78 @@ defmodule Bintreeviz.AsciiRenderer do
   alias Bintreeviz.Node
 
   def render(%Node{} = root) do
-    buffer = Textmatrix.new()
-    buffer = do_render(root, buffer)
-    IO.puts(buffer)
+    Textmatrix.new()
+    |> do_render(root)
+    |> IO.puts()
   end
 
-  defp do_render(nil, buffer), do: buffer
+  defp do_render(buffer, nil), do: buffer
 
-  defp do_render(%Node{} = root, buffer) do
-    line = make_line_for_node(root)
+  defp do_render(buffer, %Node{} = root) do
+    top_line = make_line_for_node(root, :top)
+    bottom_line = make_line_for_node(root, :bottom)
 
     buffer =
       buffer
-      |> Textmatrix.write(root.x + @offset_x, root.y + @offset_y, line)
-      |> Textmatrix.write(root.x + @offset_x, root.y + @offset_y + 1, "| #{root.label} |")
-      |> Textmatrix.write(root.x + @offset_x, root.y + 2 + @offset_y, line)
+      |> Textmatrix.write(root.x + @offset_x, root.y + @offset_y, top_line)
+      |> Textmatrix.write(root.x + @offset_x, root.y + @offset_y + 1, "┃ #{root.label} ┃")
+      |> Textmatrix.write(root.x + @offset_x, root.y + 2 + @offset_y, bottom_line)
 
-    buffer = do_render(root.left_child, buffer)
-    do_render(root.right_child, buffer)
+    buffer
+    |> do_render(root.left_child)
+    |> do_render(root.right_child)
+    |> connect(root)
   end
 
-  defp make_line_for_node(%Node{} = node) do
+  defp connect(buffer, %Node{left_child: nil, right_child: nil}), do: buffer
+
+  defp connect(buffer, %Node{} = node) do
+    case node do
+      %Node{left_child: %Node{} = left_child, right_child: %Node{} = right_child} ->
+        buffer
+        |> connect(node, left_child)
+        |> connect(node, right_child)
+
+      %Node{left_child: %Node{} = left_child, right_child: nil} ->
+        connect(buffer, node, left_child)
+
+      %Node{left_child: nil, right_child: %Node{} = right_child} ->
+        connect(buffer, node, right_child)
+    end
+  end
+
+  defp connect(buffer, %Node{} = root, %Node{} = child) do
+    root_anchor_x = (node_width(root) / 2 + root.x) |> floor()
+    root_anchor_y = root.y + 3
+
+    child_anchor_x = (node_width(child) / 2 + child.x) |> floor()
+    child_anchor_y = child.y + 1
+
+    buffer
+    |> Textmatrix.write(root_anchor_x, root_anchor_y, "┳")
+    |> Textmatrix.write(root_anchor_x, root_anchor_y + 1, "┻")
+    |> Textmatrix.write(child_anchor_x, child_anchor_y - 1, "┳")
+    |> Textmatrix.write(child_anchor_x, child_anchor_y, "┻")
+  end
+
+  defp make_line_for_node(%Node{} = node, :top) do
     line =
       node
       |> node_width()
       |> Kernel.-(2)
-      |> repeat_char("-")
+      |> repeat_char("━")
 
-    "+#{line}+"
+    "┏#{line}┓"
+  end
+
+  defp make_line_for_node(%Node{} = node, :bottom) do
+    line =
+      node
+      |> node_width()
+      |> Kernel.-(2)
+      |> repeat_char("━")
+
+    "┗#{line}┛"
   end
 
   # return the node's label length + padding + border.
