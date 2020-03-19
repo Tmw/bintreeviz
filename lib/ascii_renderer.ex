dd69b58e6e85f0786bcb18f5291382622f3e2757
Tiemen Waterreus <tiemen@tiemenwaterreus.com>
2020-03-19 22:55:28 +0100

    Refactor ASCII renderer a bit

--- lib/ascii_renderer.ex
+++ lib/ascii_renderer.ex
@@ -9,25 +9,30 @@ defmodule Bintreeviz.AsciiRenderer do
 
   @spec render(Node.t()) :: String.t()
   def render(%Node{} = root) do
-    do_render(Textmatrix.new(), root)
+    render(Textmatrix.new(), root)
   end
 
-  defp do_render(buffer, nil), do: buffer
+  defp render(buffer, nil), do: buffer
 
-  defp do_render(buffer, %Node{} = root) do
-    top_line = make_line_for_node(root, :top)
-    bottom_line = make_line_for_node(root, :bottom)
+  defp render(buffer, %Node{} = root) do
+    buffer
+    |> box_node(root)
+    |> render(root.left_child)
+    |> render(root.right_child)
+    |> connect(root)
+  end
 
-    buffer =
-      buffer
-      |> Textmatrix.write(root.x, root.y, top_line)
-      |> Textmatrix.write(root.x, root.y + 1, "┃ #{root.label} ┃")
-      |> Textmatrix.write(root.x, root.y + 2, bottom_line)
+  defp box_node(buffer, %Node{} = root) do
+    horizontal_line =
+      root
+      |> Node.width()
+      |> Kernel.-(2)
+      |> repeat_char("━")
 
     buffer
-    |> do_render(root.left_child)
-    |> do_render(root.right_child)
-    |> connect(root)
+    |> Textmatrix.write(root.x, root.y, "┏#{horizontal_line}┓")
+    |> Textmatrix.write(root.x, root.y + 1, "┃ #{root.label} ┃")
+    |> Textmatrix.write(root.x, root.y + 2, "┗#{horizontal_line}┛")
   end
 
   defp connect(buffer, %Node{left_child: nil, right_child: nil}), do: buffer
@@ -48,10 +53,10 @@ defmodule Bintreeviz.AsciiRenderer do
   end
 
   defp connect(buffer, %Node{} = root, %Node{} = child) do
-    root_anchor_x = (node_width(root) / 2 + root.x) |> floor()
+    root_anchor_x = floor(Node.width(root) / 2 + root.x)
     root_anchor_y = root.y + 2
 
-    child_anchor_x = (node_width(child) / 2 + child.x) |> floor()
+    child_anchor_x = floor(Node.width(child) / 2 + child.x)
     child_anchor_y = child.y
 
     # generate the horizontal connecting line between node's anchor points
@@ -61,70 +66,48 @@ defmodule Bintreeviz.AsciiRenderer do
       |> Kernel.-(1)
       |> repeat_char("━")
 
-    # decide on parent's corner piece
-    parent_corner_piece =
-      case root do
-        %Node{left_child: %Node{} = _left_child, right_child: %Node{} = _right_child} ->
-          # Got two children
-          "┻"
-
-        %Node{left_child: %Node{} = _left_child, right_child: nil} ->
-          # Got only left child
-          "┛"
-
-        %Node{left_child: nil, right_child: %Node{} = _right_child} ->
-          # Got only right child
-          "┗"
-      end
-
     # decide which way the corner pieces should face
-    child_corner_pieces =
-      cond do
-        root_anchor_x > child_anchor_x ->
-          # child is left
-          [top: "┛", bottom: "┏"]
-
-        child_anchor_x > root_anchor_x ->
-          # child is right
-          [top: "┗", bottom: "┓"]
-      end
+    child_connection_char = child_connection_char(root_anchor_x, child_anchor_x)
 
     buffer
     # draw connecting corner pieces for parent
     |> Textmatrix.write(root_anchor_x, root_anchor_y, "┳")
-    |> Textmatrix.write(root_anchor_x, root_anchor_y + 1, parent_corner_piece)
+    |> Textmatrix.write(root_anchor_x, root_anchor_y + 1, parent_connection_char(root))
 
     # draw connecting corner pices
-    |> Textmatrix.write(child_anchor_x, child_anchor_y - 1, child_corner_pieces[:bottom])
+    |> Textmatrix.write(child_anchor_x, child_anchor_y - 1, child_connection_char)
     |> Textmatrix.write(child_anchor_x, child_anchor_y, "┻")
 
     # draw connecting horizontal line
     |> Textmatrix.write(min(root_anchor_x, child_anchor_x) + 1, root_anchor_y + 1, "#{line}")
   end
 
-  defp make_line_for_node(%Node{} = node, :top) do
-    line =
-      node
-      |> node_width()
-      |> Kernel.-(2)
-      |> repeat_char("━")
+  defp parent_connection_char(%Node{} = root) do
+    case root do
+      %Node{left_child: %Node{} = _left_child, right_child: %Node{} = _right_child} ->
+        # Got two children
+        "┻"
 
-    "┏#{line}┓"
+      %Node{left_child: %Node{} = _left_child, right_child: nil} ->
+        # Got only left child
+        "┛"
+
+      %Node{left_child: nil, right_child: %Node{} = _right_child} ->
+        # Got only right child
+        "┗"
+    end
   end
 
-  defp make_line_for_node(%Node{} = node, :bottom) do
-    line =
-      node
-      |> node_width()
-      |> Kernel.-(2)
-      |> repeat_char("━")
-
-    "┗#{line}┛"
+  defp child_connection_char(root_anchor_x, child_anchor_x)
+       when root_anchor_x > child_anchor_x do
+    # child is ofsetted to the left
+    "┏"
   end
 
-  # return the node's label length + padding + border.
-  defp node_width(%Node{} = node) do
-    Node.width(node)
+  defp child_connection_char(root_anchor_x, child_anchor_x)
+       when root_anchor_x < child_anchor_x do
+    # child is offsetted to the right
+    "┓"
   end
 
   defp repeat_char(times, char) do
